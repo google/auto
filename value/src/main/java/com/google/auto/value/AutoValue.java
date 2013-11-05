ed2da4bdebd2a4331b98766a7b1f9feb4dec5a02
Gregory Kick <gak@google.com>
2013-11-05 12:54:27 -0600

    Revise the @AutoValue docs.
    Removing the factory example, using the new links, adding moe stripping, simplifying, etc.
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=56095477

--- value/src/main/java/com/google/auto/value/AutoValue.java
+++ value/src/main/java/com/google/auto/value/AutoValue.java
@@ -1,17 +1,15 @@
 /*
  * Copyright (C) 2012 Google, Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.google.auto.value;
 
@@ -21,52 +19,29 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Annotation indicating that the annotated type is a value type with an automatically-generated
- * implementation.
+ * Specifies that <a href="http://goo.gl/Ter394">AutoValue</a> should generate an implementation
+ * class for the annotated abstract class, implementing the standard {@link Object} methods like
+ * {@link Object#equals equals} to have conventional value semantics. A simple example: <pre>
  *
- * <p>Here is a simple example of a value type using {@code @AutoValue}:
+ *   &#64;AutoValue
+ *   abstract class Person {
+ *     static Person create(String name, int id) {
+ *       return new AutoValue_Person(name, id);
+ *     }
  *
- * <pre>
- * {@code @AutoValue}
- * {@code public abstract class Contact {
- *   public abstract String name();
- *   public abstract List<String> phoneNumbers();
- *   public abstract int sortOrder();
+ *     abstract String name();
+ *     abstract int id();
+ *   }</pre>
  *
- *   public static Contact create(String name, List<String> phoneNumbers, int sortOrder) {
- *     return new AutoValue_Contact(name, phoneNumbers, sortOrder);
- *   }
- * }}</pre>
+ * @see <a href="http://goo.gl/Ter394">AutoValue User's Guide</a>
  *
- * <p>The generated subclass is called {@code AutoValue_Contact} but users of {@code Contact} do not
- * need to know that. Only the implementation of the {@code create} method does.
- *
- * <p>As an alternative to instantiating the generated subclass in the {@code create} method, you
- * can define a {@code Factory} interface like this:</p>
- *
- * <pre>
- * {@code @AutoValue}
- * {@code public abstract class Contact {
- *   public abstract String name();
- *   public abstract List<String> phoneNumbers();
- *   public abstract int sortOrder();
- *
- *   public static Contact create(String name, List<String> phoneNumbers, int sortOrder) {
- *     return AutoValues.using(Factory.class).create(name, phoneNumbers, sortOrder);
- *   }
- *
- *   interface Factory {
- *     Contact create(String name, List<String> phoneNumbers, int sortOrder);
- *   }
- * }}</pre>
+ * <!-- MOE:begin_intracomment_strip -->
+ * @see <a href="http://go/autovalue">Additional Google-internal information<a/>
+ * <!-- MOE:end_intracomment_strip -->
  *
  * @author Éamonn McManus
- */
-/*
- *  TODO(gak): We should obviously try to figure out whether this whole factory mechanism can be
- *  replaced with @AutoFactory
+ * @author Kevin Bourrillion
  */
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.TYPE)
-public @interface AutoValue {
-}
+public @interface AutoValue {}
