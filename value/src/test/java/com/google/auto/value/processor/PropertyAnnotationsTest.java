53300e4d1f427ce2cfae966d2c140a4665861aed
Javier Campanini <jcampanini@palantir.com>
2014-08-20 10:51:34 +0100

    refactored annotation tests to minimize duplicate code.

--- value/src/test/java/com/google/auto/value/processor/PropertyAnnotationsTest.java
+++ value/src/test/java/com/google/auto/value/processor/PropertyAnnotationsTest.java
@@ -1,5 +1,7 @@
 package com.google.auto.value.processor;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import com.google.testing.compile.JavaFileObjects;
 import junit.framework.TestCase;
 
@@ -15,67 +17,94 @@ import static org.truth0.Truth.ASSERT;
  */
 public class PropertyAnnotationsTest extends TestCase {
 
-  public void testCompilationWithBasicAnnotation() {
-    JavaFileObject javaFileObject = JavaFileObjects.forSourceLines(
-        "foo.bar.Baz",
-        "package foo.bar;",
-        "",
-        "import com.google.auto.value.AutoValue;",
-        "import javax.annotation.Nullable;",
-        "",
-        "@AutoValue",
-        "public abstract class Baz {",
-        "  @Nullable",
-        "  public abstract int buh();",
-        "",
-        "  public static Baz create(int buh) {",
-        "    return new AutoValue_Baz(buh);",
-        "  }",
-        "}");
-    JavaFileObject expectedOutput = JavaFileObjects.forSourceLines(
-        "foo.bar.AutoValue_Baz",
-        "package foo.bar;",
-        "",
-        "import javax.annotation.Generated;",
-        "",
-        "@Generated(\"" + AutoValueProcessor.class.getName() + "\")",
-        "final class AutoValue_Baz extends Baz {",
-        "  private final int buh;",
-        "",
-        "  AutoValue_Baz(int buh) {",
-        "    this.buh = buh;",
-        "  }",
-        "",
-        "  @javax.annotation.Nullable",
-        "  @Override public int buh() {",
-        "    return buh;",
-        "  }",
-        "",
-        "  @Override public String toString() {",
-        "    return \"Baz{\"",
-        "        + \"buh=\" + buh",
-        "        + \"}\";",
-        "  }",
-        "",
-        "  @Override public boolean equals(Object o) {",
-        "    if (o == this) {",
-        "      return true;",
-        "    }",
-        "    if (o instanceof Baz) {",
-        "      Baz that = (Baz) o;",
-        "      return (this.buh == that.buh());",
-        "    }",
-        "    return false;",
-        "  }",
-        "",
-        "  @Override public int hashCode() {",
-        "    int h = 1;",
-        "    h *= 1000003;",
-        "    h ^= buh;",
-        "    return h;",
-        "  }",
-        "}"
-    );
+  private JavaFileObject sourceCode(Iterable<String> imports, Iterable<String> annotations) {
+    ImmutableList<String> list = ImmutableList.<String>builder()
+        .add(
+            "package foo.bar;",
+            "",
+            "import com.google.auto.value.AutoValue;"
+        )
+        .addAll(imports)
+        .add(
+            "",
+            "@AutoValue",
+            "public abstract class Baz {"
+        )
+        .addAll(annotations)
+        .add(
+            "  public abstract int buh();",
+            "",
+            "  public static Baz create(int buh) {",
+            "    return new AutoValue_Baz(buh);",
+            "  }",
+            "}"
+        )
+        .build();
+
+    String[] lines = list.toArray(new String[list.size()]);
+    return JavaFileObjects.forSourceLines("foo.bar.Baz", lines);
+  }
+
+  private JavaFileObject expectedCode(Iterable<String> annotations) {
+    ImmutableList<String> list = ImmutableList.<String>builder()
+        .add(
+            "package foo.bar;",
+            "",
+            "import javax.annotation.Generated;",
+            "",
+            "@Generated(\"" + AutoValueProcessor.class.getName() + "\")",
+            "final class AutoValue_Baz extends Baz {",
+            "  private final int buh;",
+            "",
+            "  AutoValue_Baz(int buh) {",
+            "    this.buh = buh;",
+            "  }",
+            ""
+        )
+        .addAll(annotations)
+        .add(
+            "  @Override public int buh() {",
+            "    return buh;",
+            "  }",
+            "",
+            "  @Override public String toString() {",
+            "    return \"Baz{\"",
+            "        + \"buh=\" + buh",
+            "        + \"}\";",
+            "  }",
+            "",
+            "  @Override public boolean equals(Object o) {",
+            "    if (o == this) {",
+            "      return true;",
+            "    }",
+            "    if (o instanceof Baz) {",
+            "      Baz that = (Baz) o;",
+            "      return (this.buh == that.buh());",
+            "    }",
+            "    return false;",
+            "  }",
+            "",
+            "  @Override public int hashCode() {",
+            "    int h = 1;",
+            "    h *= 1000003;",
+            "    h ^= buh;",
+            "    return h;",
+            "  }",
+            "}"
+        )
+        .build();
+
+    String[] lines = list.toArray(new String[list.size()]);
+    return JavaFileObjects.forSourceLines("foo.bar.AutoValue_Baz", lines);
+  }
+
+  private void assertGeneratedMatches(
+      Iterable<String> imports,
+      Iterable<String> annotations,
+      Iterable<String> expectedAnnotations) {
+
+    JavaFileObject javaFileObject = sourceCode(imports, annotations);
+    JavaFileObject expectedOutput = expectedCode(expectedAnnotations);
 
     ASSERT.about(javaSource())
         .that(javaFileObject)
@@ -84,141 +113,24 @@ public class PropertyAnnotationsTest extends TestCase {
         .and().generatesSources(expectedOutput);
   }
 
-  public void testCompilationWithSingleValueAnnotation() {
-    JavaFileObject javaFileObject = JavaFileObjects.forSourceLines(
-        "foo.bar.Baz",
-        "package foo.bar;",
-        "",
-        "import com.google.auto.value.AutoValue;",
-        "import javax.annotation.Nullable;",
-        "",
-        "@AutoValue",
-        "public abstract class Baz {",
-        "  @SuppressWarnings(\"a\")",
-        "  public abstract int buh();",
-        "",
-        "  public static Baz create(int buh) {",
-        "    return new AutoValue_Baz(buh);",
-        "  }",
-        "}");
-    JavaFileObject expectedOutput = JavaFileObjects.forSourceLines(
-        "foo.bar.AutoValue_Baz",
-        "package foo.bar;",
-        "",
-        "import javax.annotation.Generated;",
-        "",
-        "@Generated(\"" + AutoValueProcessor.class.getName() + "\")",
-        "final class AutoValue_Baz extends Baz {",
-        "  private final int buh;",
-        "",
-        "  AutoValue_Baz(int buh) {",
-        "    this.buh = buh;",
-        "  }",
-        "",
-        "  @java.lang.SuppressWarnings(value={\"a\"})",
-        "  @Override public int buh() {",
-        "    return buh;",
-        "  }",
-        "",
-        "  @Override public String toString() {",
-        "    return \"Baz{\"",
-        "        + \"buh=\" + buh",
-        "        + \"}\";",
-        "  }",
-        "",
-        "  @Override public boolean equals(Object o) {",
-        "    if (o == this) {",
-        "      return true;",
-        "    }",
-        "    if (o instanceof Baz) {",
-        "      Baz that = (Baz) o;",
-        "      return (this.buh == that.buh());",
-        "    }",
-        "    return false;",
-        "  }",
-        "",
-        "  @Override public int hashCode() {",
-        "    int h = 1;",
-        "    h *= 1000003;",
-        "    h ^= buh;",
-        "    return h;",
-        "  }",
-        "}"
-    );
-
-    ASSERT.about(javaSource())
-        .that(javaFileObject)
-        .processedWith(new AutoValueProcessor())
-        .compilesWithoutError()
-        .and().generatesSources(expectedOutput);
+  public void testSimpleAnnotation() {
+    assertGeneratedMatches(
+        Lists.newArrayList("import javax.annotation.Nullable;"),
+        Lists.newArrayList("@Nullable"),
+        Lists.newArrayList("@javax.annotation.Nullable"));
   }
 
-  public void testCompilationWithManyValuedAnnotation() {
-    JavaFileObject javaFileObject = JavaFileObjects.forSourceLines(
-        "foo.bar.Baz",
-        "package foo.bar;",
-        "",
-        "import com.google.auto.value.AutoValue;",
-        "import javax.annotation.Nullable;",
-        "",
-        "@AutoValue",
-        "public abstract class Baz {",
-        "  @SuppressWarnings({\"a\", \"b\"})",
-        "  public abstract int buh();",
-        "",
-        "  public static Baz create(int buh) {",
-        "    return new AutoValue_Baz(buh);",
-        "  }",
-        "}");
-    JavaFileObject expectedOutput = JavaFileObjects.forSourceLines(
-        "foo.bar.AutoValue_Baz",
-        "package foo.bar;",
-        "",
-        "import javax.annotation.Generated;",
-        "",
-        "@Generated(\"" + AutoValueProcessor.class.getName() + "\")",
-        "final class AutoValue_Baz extends Baz {",
-        "  private final int buh;",
-        "",
-        "  AutoValue_Baz(int buh) {",
-        "    this.buh = buh;",
-        "  }",
-        "",
-        "  @java.lang.SuppressWarnings(value={\"a\", \"b\"})",
-        "  @Override public int buh() {",
-        "    return buh;",
-        "  }",
-        "",
-        "  @Override public String toString() {",
-        "    return \"Baz{\"",
-        "        + \"buh=\" + buh",
-        "        + \"}\";",
-        "  }",
-        "",
-        "  @Override public boolean equals(Object o) {",
-        "    if (o == this) {",
-        "      return true;",
-        "    }",
-        "    if (o instanceof Baz) {",
-        "      Baz that = (Baz) o;",
-        "      return (this.buh == that.buh());",
-        "    }",
-        "    return false;",
-        "  }",
-        "",
-        "  @Override public int hashCode() {",
-        "    int h = 1;",
-        "    h *= 1000003;",
-        "    h ^= buh;",
-        "    return h;",
-        "  }",
-        "}"
-    );
+  public void testSingleStringValueAnnotation() {
+    assertGeneratedMatches(
+        Lists.<String>newArrayList(),
+        Lists.newArrayList("@SuppressWarnings(\"a\")"),
+        Lists.newArrayList("@java.lang.SuppressWarnings(value={\"a\"})"));
+  }
 
-    ASSERT.about(javaSource())
-        .that(javaFileObject)
-        .processedWith(new AutoValueProcessor())
-        .compilesWithoutError()
-        .and().generatesSources(expectedOutput);
+  public void testMultiStringValueAnnotation() {
+    assertGeneratedMatches(
+        Lists.<String>newArrayList(),
+        Lists.newArrayList("@SuppressWarnings({\"a\", \"b\"})"),
+        Lists.newArrayList("@java.lang.SuppressWarnings(value={\"a\", \"b\"})"));
   }
 }
