05d799b756d9cc1229ff710d638363b297e267d6
Dmitry Kosolobov <kosolobovdima@gmail.com>
2015-01-31 16:40:07 +0300

    AutoService is not necessarily should leave traces in class files!

--- service/src/main/java/com/google/auto/service/AutoService.java
+++ service/src/main/java/com/google/auto/service/AutoService.java
@@ -16,9 +16,11 @@
 package com.google.auto.service;
 
 import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Target;
+import java.lang.annotation.Retention;
 
 /**
  * An annotation for service providers as described in {@link java.util.ServiceLoader}. The
@@ -34,6 +36,7 @@ import java.lang.annotation.Target;
  * </ul>
  */
 @Documented
+@Retention(SOURCE)
 @Target(TYPE)
 public @interface AutoService {
   /** Returns the interface implemented by this service provider. */
