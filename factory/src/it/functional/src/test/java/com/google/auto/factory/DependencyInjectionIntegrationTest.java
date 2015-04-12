5f114e8effe25c1ed96c79232cb08c4f4ad933d3
Thomas Broyer <t.broyer@ltgt.net>
2015-04-12 23:25:46 +0200

    Fix AutoFactory build

--- factory/src/it/functional/src/test/java/com/google/auto/factory/DependencyInjectionIntegrationTest.java
+++ factory/src/it/functional/src/test/java/com/google/auto/factory/DependencyInjectionIntegrationTest.java
@@ -4,8 +4,6 @@ import static com.google.common.truth.Truth.assertThat;
 
 import com.google.inject.Guice;
 
-import dagger.ObjectGraph;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -14,7 +12,7 @@ import org.junit.runners.JUnit4;
 public class DependencyInjectionIntegrationTest {
   @Test public void daggerInjectedFactory() {
     FactoryGeneratedFactory factoryGeneratedFactory =
-        ObjectGraph.create(DaggerModule.class).get(FactoryGeneratedFactory.class);
+        DaggerDaggerComponent.create().factoryGeneratedFactory();
     FactoryGenerated one = factoryGeneratedFactory.create("A");
     FactoryGenerated two = factoryGeneratedFactory.create("B");
     assertThat(one.name()).isEqualTo("A");
