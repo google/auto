0a19f5158ca9429430cd88a9276a2bb2bdeea8ac
erikbodzsar <erikbodzsar@google.com>
2015-09-03 11:14:17 -0700

    Close javadoc h2 tag in javadoc. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=102261444

--- factory/src/main/java/com/google/auto/factory/AutoFactory.java
+++ factory/src/main/java/com/google/auto/factory/AutoFactory.java
@@ -23,7 +23,7 @@ import static java.lang.annotation.ElementType.TYPE;
 /**
  * An annotation to be applied to elements for which a factory should be automatically generated.
  *
- * <h2>Visibility
+ * <h2>Visibility</h2>
  * <p>The visibility of the generated factories will always be either {@code public} or default
  * visibility. The visibility of any given factory method is determined by the visibility of the
  * type being created. The generated factory is {@code public} if any of the factory methods are.
