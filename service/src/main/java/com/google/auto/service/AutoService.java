674663cf7e7f75dd369eda4991c362d067403070
emcmanus <emcmanus@google.com>
2016-01-12 10:38:41 -0800

    Give @AutoService source retention, per https://github.com/google/auto/issues/143. ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=111963998

--- service/src/main/java/com/google/auto/service/AutoService.java
+++ service/src/main/java/com/google/auto/service/AutoService.java
@@ -16,8 +16,10 @@
 package com.google.auto.service;
 
 import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 /**
@@ -34,6 +36,7 @@ import java.lang.annotation.Target;
  * </ul>
  */
 @Documented
+@Retention(SOURCE)
 @Target(TYPE)
 public @interface AutoService {
   /** Returns the interface implemented by this service provider. */
