f4d69fb6598c4316bf325e1981a881122cdb3523
emcmanus <emcmanus@google.com>
2016-05-27 15:27:02 -0700

    Make copyright notices consistent. ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=123463425

--- value/src/it/gwtserializer/src/test/java/com/google/auto/value/client/GwtSerializerTest.java
+++ value/src/it/gwtserializer/src/test/java/com/google/auto/value/client/GwtSerializerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 The Guava Authors
+ * Copyright (C) 2015 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
