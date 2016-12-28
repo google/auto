3df3be852c308ed68fc81c1e44eb5714aafcf0bf
emcmanus <emcmanus@google.com>
2016-12-28 08:35:46 -0800

    Remove a couple of references to ReflectiveOperationException, which did not exist on Java 6. Fixes https://github.com/google/auto/issues/418.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=143109682

--- value/src/main/java/com/google/auto/value/processor/escapevelocity/ReferenceNode.java
+++ value/src/main/java/com/google/auto/value/processor/escapevelocity/ReferenceNode.java
@@ -413,7 +413,7 @@ abstract class ReferenceNode extends ExpressionNode {
       String pkg = packageNameOf(c);
       Object module = CLASS_GET_MODULE_METHOD.invoke(c);
       return (Boolean) MODULE_IS_EXPORTED_METHOD.invoke(module, pkg);
-    } catch (ReflectiveOperationException e) {
+    } catch (Exception e) {
       return false;
     }
   }
@@ -428,7 +428,7 @@ abstract class ReferenceNode extends ExpressionNode {
       classGetModuleMethod = Class.class.getMethod("getModule");
       Class<?> moduleClass = classGetModuleMethod.getReturnType();
       moduleIsExportedMethod = moduleClass.getMethod("isExported", String.class);
-    } catch (ReflectiveOperationException e) {
+    } catch (Exception e) {
       classGetModuleMethod = null;
       moduleIsExportedMethod = null;
     }
