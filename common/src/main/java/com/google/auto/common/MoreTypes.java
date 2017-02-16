c0637a44dab4b0fddd4c2e0b9794db9864ecb580
ronshapiro <ronshapiro@google.com>
2017-02-16 15:28:57 -0800

    MoreTypes.asWildcard() should accept a type which isn't guaranteed to already be a WildcardType
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=147775835

--- common/src/main/java/com/google/auto/common/MoreTypes.java
+++ common/src/main/java/com/google/auto/common/MoreTypes.java
@@ -738,7 +738,7 @@ public final class MoreTypes {
    * Returns a {@link WildcardType} if the {@link TypeMirror} represents a wildcard type or throws
    * an {@link IllegalArgumentException}.
    */
-  public static WildcardType asWildcard(WildcardType maybeWildcardType) {
+  public static WildcardType asWildcard(TypeMirror maybeWildcardType) {
     return maybeWildcardType.accept(WildcardTypeVisitor.INSTANCE, null);
   }
 
