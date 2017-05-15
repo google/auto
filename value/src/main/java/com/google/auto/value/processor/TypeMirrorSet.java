cd04524bf004fc3a26e1ba57d8dfb44e121a66c2
emcmanus <emcmanus@google.com>
2017-05-15 15:32:47 -0700

    Add a new method TypeSimplifier.simplifyWithAnnotations that will include type annotations in the resultant type string. This method is not yet used. A later change will use it to include type annotations where appropriate in generated code.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=156113224

--- value/src/main/java/com/google/auto/value/processor/TypeMirrorSet.java
+++ value/src/main/java/com/google/auto/value/processor/TypeMirrorSet.java
@@ -17,6 +17,7 @@ package com.google.auto.value.processor;
 
 import com.google.auto.common.MoreTypes;
 import com.google.common.base.Equivalence;
+import com.google.common.collect.ImmutableList;
 import java.util.AbstractSet;
 import java.util.Collection;
 import java.util.HashSet;
@@ -38,6 +39,10 @@ class TypeMirrorSet extends AbstractSet<TypeMirror> {
 
   TypeMirrorSet(Collection<? extends TypeMirror> types) {
     addAll(types);
+  }
+
+  static TypeMirrorSet of(TypeMirror... types) {
+    return new TypeMirrorSet(ImmutableList.copyOf(types));
   }
 
   private Equivalence.Wrapper<TypeMirror> wrap(TypeMirror typeMirror) {
