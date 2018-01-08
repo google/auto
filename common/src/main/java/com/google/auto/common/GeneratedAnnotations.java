0383c1cf7472d7444463353aa2ac956c9471130a
cushon <cushon@google.com>
2018-01-08 09:17:34 -0800

    Add an implementation of generatedAnnotation that uses the source version instead of classpath introspection.
    RELNOTES=N/A
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=181173931

--- common/src/main/java/com/google/auto/common/GeneratedAnnotations.java
+++ common/src/main/java/com/google/auto/common/GeneratedAnnotations.java
@@ -16,6 +16,7 @@
 package com.google.auto.common;
 
 import java.util.Optional;
+import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.util.Elements;
 
@@ -30,12 +31,32 @@ public final class GeneratedAnnotations {
    * <p>First looks for {@code javax.annotation.processing.Generated}, and then {@code
    * javax.annotation.Generated}. Returns whichever is in the classpath (or modulepath), or {@link
    * Optional#empty()} if neither is.
+   *
+   * @deprecated prefer {@link #generatedAnnotation(Elements, SourceVersion)}
    */
+  @Deprecated
   public static Optional<TypeElement> generatedAnnotation(Elements elements) {
     TypeElement jdk9Generated = elements.getTypeElement("javax.annotation.processing.Generated");
     if (jdk9Generated != null) {
       return Optional.of(jdk9Generated);
     }
     return Optional.ofNullable(elements.getTypeElement("javax.annotation.Generated"));
+  }
+
+  /**
+   * Returns the element corresponding to the {@code @Generated} annotation present at the target
+   * {@code SourceVersion}.
+   *
+   * <p>Returns {@code javax.annotation.processing.Generated} for JDK 9 and newer, {@code
+   * javax.annotation.Generated} for earlier releases, and Optional#empty()} if the annotation is
+   * not available.
+   */
+  public static Optional<TypeElement> generatedAnnotation(
+      Elements elements, SourceVersion sourceVersion) {
+    return Optional.ofNullable(
+        elements.getTypeElement(
+            sourceVersion.compareTo(SourceVersion.RELEASE_8) > 0
+                ? "javax.annotation.processing.Generated"
+                : "javax.annotation.Generated"));
   }
 }
