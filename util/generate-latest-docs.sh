99a6b31dd5b0048610df31c88cd081b6e791b082
Colin Decker <cgdecker@google.com>
2021-03-16 13:29:05 -0700

    Switch auto from Travis to GitHub Actions and start testing on JDK 15.
    RELNOTES=n/a
    PiperOrigin-RevId: 363257277

--- util/generate-latest-docs.sh
+++ util/generate-latest-docs.sh
@@ -1,25 +1,22 @@
-# see http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/ for details
+#!/bin/bash
 
-if [ "$TRAVIS_REPO_SLUG" == "google/auto" ] && \
-   [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && \
-   [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
-   [ "$TRAVIS_BRANCH" == "master" ]; then
-  echo -e "Publishing javadoc...\n"
-  
-  mvn -f build-pom.xml javadoc:aggregate
-  TARGET="$(pwd)/target"
+# Run by GitHub Actions (see .github/workflows/ci.yml)
 
-  cd $HOME
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/google/auto gh-pages > /dev/null
-  
-  cd gh-pages
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "travis-ci"
-  git rm -rf api/latest 
-  mv ${TARGET}/site/apidocs api/latest
-  git add -A -f api/latest
-  git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
+echo -e "Publishing javadoc...\n"
 
-  echo -e "Published Javadoc to gh-pages.\n"
-fi
+mvn -f build-pom.xml javadoc:aggregate
+TARGET="$(pwd)/target"
+
+cd $HOME
+git clone --quiet --branch=gh-pages "https://x-access-token:${GITHUB_TOKEN}@github.com/google/auto" gh-pages > /dev/null
+
+cd gh-pages
+git config --global user.name "$GITHUB_ACTOR"
+git config --global user.email "$GITHUB_ACTOR@users.noreply.github.com"
+git rm -rf api/latest
+mv ${TARGET}/site/apidocs api/latest
+git add -A -f api/latest
+git commit -m "Latest javadoc on successful CI build auto-pushed to gh-pages"
+git push -fq origin gh-pages > /dev/null
+
+echo -e "Published Javadoc to gh-pages.\n"
