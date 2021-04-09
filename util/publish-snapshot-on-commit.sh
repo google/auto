613a61af88b34d22ffa39b7d47b2a512ac79ab39
Colin Decker <cgdecker@google.com>
2021-04-09 11:19:26 -0700

    Skip javadoc jar in snapshot deployment for now, as it fails for some reason.
    RELNOTES=n/a
    PiperOrigin-RevId: 367668858

--- util/publish-snapshot-on-commit.sh
+++ util/publish-snapshot-on-commit.sh
@@ -3,4 +3,4 @@
 set -e
 
 mvn -B dependency:go-offline test clean -U --quiet --fail-never -DskipTests=true -f build-pom.xml
-mvn source:jar javadoc:jar deploy -DskipTests=true -f build-pom.xml
+mvn -B -U source:jar deploy -DskipTests=true -f build-pom.xml
