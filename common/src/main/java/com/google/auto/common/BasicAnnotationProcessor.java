56f521e8dc49e2becb126932d45f456e33eb6309
Aria <25481147+AriaAdibi@users.noreply.github.com>
2022-04-29 10:41:01 -0700

    Suggesting a rewording of `ElementName` Javadoc
    This is a proposed rewording of the Javadoc of `ElementName`, based on the quesion #1291.
    
    Fixes #1309
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/google/auto/pull/1309 from AriaAdibi:dev/javadoc-rewording-question#1291 b56ceb65327947d6bbc4b1e1769763e326519ec6
    PiperOrigin-RevId: 445454959

--- common/src/main/java/com/google/auto/common/BasicAnnotationProcessor.java
+++ common/src/main/java/com/google/auto/common/BasicAnnotationProcessor.java
@@ -179,9 +179,7 @@ public abstract class BasicAnnotationProcessor extends AbstractProcessor {
   @Override
   public final ImmutableSet<String> getSupportedAnnotationTypes() {
     checkState(steps != null);
-    return steps.stream()
-        .flatMap(step -> step.annotations().stream())
-        .collect(toImmutableSet());
+    return steps.stream().flatMap(step -> step.annotations().stream()).collect(toImmutableSet());
   }
 
   @Override
@@ -541,6 +539,9 @@ public abstract class BasicAnnotationProcessor extends AbstractProcessor {
      * An {@link ElementName} for an annotated element. If {@code element} is a package, uses the
      * fully qualified name of the package. If it's a type, uses its fully qualified name.
      * Otherwise, uses the fully-qualified name of the nearest enclosing type.
+     *
+     * <p>A package can be annotated if it has a {@code package-info.java} with annotations on the
+     * package declaration.
      */
     static ElementName forAnnotatedElement(Element element) {
       return element.getKind() == PACKAGE
