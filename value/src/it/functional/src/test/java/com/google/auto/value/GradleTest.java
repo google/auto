b2fcef5157e493b7695974fe7564f5035145846d
Éamonn McManus <emcmanus@google.com>
2023-02-19 12:55:02 -0800

    Really reset the version, and adjust listing code.

--- value/src/it/functional/src/test/java/com/google/auto/value/GradleTest.java
+++ value/src/it/functional/src/test/java/com/google/auto/value/GradleTest.java
@@ -85,8 +85,11 @@ public class GradleTest {
     Path autoValuePath = localRepositoryPath.resolve("com/google/auto/value/auto-value");
     assertThat(Files.exists(autoValuePath)).isTrue();
     Path autoValueVersionPath = autoValuePath.resolve(autoValueVersion);
-    assertThat(Files.exists(autoValueVersionPath)).isTrue();
-    Files.list(autoValueVersionPath).forEach(p -> System.err.printf("@@@ %s\n", p));
+    if (Files.exists(autoValueVersionPath)) {
+      Files.list(autoValueVersionPath).forEach(p -> System.err.printf("@@@ %s\n", p));
+    } else {
+      Files.list(autoValuePath).forEach(p -> System.err.printf("@@@ %s\n", p));
+    }
 
     // Set up the fake Gradle project.
     String buildGradleText = expandSystemProperties(BUILD_GRADLE_TEXT);
