1cfd830accf94f53e50ffa016800293d2927c79d
cpovirk <cpovirk@google.com>
2025-09-16 22:04:42 -0700

    Stop requesting a source jar at the command line.
    Now that our builds generate a source jar by default (cl/807859217), our command-line request leads to an error about duplicate jars:
    
    ```
    Error:  We have duplicated artifacts attached.
    ...
    Error:  Failed to execute goal org.apache.maven.plugins:maven-source-plugin:3.3.1:jar-no-fork (attach-sources) on project auto-common: Presumably you have configured maven-source-plugin to execute twice in your build. You have to configure a classifier for at least one of them. -> [Help 1]
    ```
    
    Compare cl/807772725 for Truth (and cl/807859217 for most of our projects, though sadly not Auto).
    
    Fixes (he said, optimistically) https://github.com/google/auto/issues/1965
    
    RELNOTES=n/a
    PiperOrigin-RevId: 807981919

--- util/publish-snapshot-on-commit.sh
+++ util/publish-snapshot-on-commit.sh
@@ -3,4 +3,4 @@
 set -e
 
 mvn -B dependency:go-offline test clean -U --quiet --fail-never -DskipTests=true -f build-pom.xml
-mvn -B -U source:jar deploy -DskipTests=true -f build-pom.xml
+mvn -B -U deploy -DskipTests=true -f build-pom.xml
